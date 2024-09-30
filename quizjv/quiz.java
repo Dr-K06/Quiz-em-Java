b27673b421a3154dc7c4abc5b7ac7a34fad2786f
Dr-K06 <carloseduardosousacostatj@gmail.com>
2024-09-30 15:44:09 -0300

    Update quiz.java

--- quizjv/quiz.java
+++ quizjv/quiz.java
@@ -6,7 +6,9 @@ public class Quiz {
         String[] questions = {
             "1. Qual é a capital da França?\n a) Londres\n b) Berlim\n c) Paris\n d) Madrid",
             "2. Qual é a capital do Brasil?\n a) Buenos Aires\n b) Londres\n c) Brasília\n d) Rio de Janeiro",
-           
+           "3. Qual é a capital dos Estados Unidos?\n a) Nova York\n b) Washington\n c) Chicago \n d) Londres",
+            "4. Qual é a capital do México?\n a) Cidade do México\n b) Cancún\n c) Distrito Federal\n d) Novo México",
+            "5. Qual é a capital da Holanda?\n a) Haarlem\n b) Amsterdã\n c) Moscou\n d) Berlim",
         };
 
         char[] answers = {'c', 'c', 'b', 'a', 'b', 'a', 'b', 'b', 'c', 'c','d','a','a','a','c'};
