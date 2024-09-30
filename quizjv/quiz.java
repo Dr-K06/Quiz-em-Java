c792480628bec6ca0d7e81a34c71822641d4f56e
Dr-K06 <carloseduardosousacostatj@gmail.com>
2024-09-30 15:57:16 -0300

    Update quiz.java

--- quizjv/quiz.java
+++ quizjv/quiz.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Quiz {
+public class quiz {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String[] questions = {
